f77ec089827a387d5a83fa6fe7a8d261f817a2d8
Matthieu Mota <matthieumota@gmail.com>
2023-03-06 11:26:09 +0100

    Static

---
+++ 05-static/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Require toutes les classes d'un seul coup
+spl_autoload_register(function ($class) {
+    // Je "triche" sur le nom de la classe
+    // M2i\F1 correspond à src
+    $class = str_replace('M2i\\F1\\', '', $class);
+
+    require __DIR__.'/src/'.$class.'.php';
+});
+
+use M2i\F1\Formula;
+
+$f1 = new Formula('F1');
+echo Formula::$count.'<br />'; // 1
+$f2 = new Formula('F2');
+$f3 = new Formula('F3');
+echo Formula::$count.'<br />'; // 3
+echo Formula::howMany().'<br />';
+echo Formula::WHEELS.' roues <br />';
+var_dump($f1);
+var_dump($f2);
+var_dump($f3);
---
+++ 05-static/src/Formula.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace M2i\F1;
+// Le namespace est déclaré avant la classe
+// Il permet de "ranger" sa classe
+
+class Formula
+{
+    public $name;
+    public static $count = 0;
+    public const WHEELS = 4;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        self::$count++; // ou static::$count++;
+    }
+
+    public static function howMany()
+    {
+        return 'Sur la piste, il y a '.self::$count.' formules.';
+    }
+}
